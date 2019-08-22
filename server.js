b0fe37dac70e0973a754e2cff82c08e6faf0f6a2
lanceliamll <lancelliotqqq@gmail.com>
2019-08-22 18:50:46 +0800

    Added models

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+//Connect to Mongo
+connectDB();
 
 const app = express();
 
+//Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server Started on ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server Started on http://localhost:${PORT}`)
+);
