2f9bde00433590abd84f739a6c1a3c0c2446729e
lanceliamll <lancelliotqqq@gmail.com>
2019-08-22 18:28:45 +0800

    Init

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "inferno";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
